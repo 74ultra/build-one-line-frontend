d7871b55c02a31ba38c76604ba44c87034bb085b
Leon Chisum <leonchisum3306@gmail.com>
2019-11-14 12:39:24 -0500

    PostLists component rendering posts w/delete feat.

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const PostCard = props => {
+// components
+import DeleteBtn from '../Buttons/DeleteBtn';
 
+const PostCard = props => {
+    const { id, date, body } = props.post
+    
     return (
-        <div>PostCard.js</div>
+     <section>
+        <div> { date } </div>
+        <div> { body } </div>
+        <DeleteBtn id={ id } delPost={ props.delPost } />
+     </section>
     )
 }
 
--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { postsEx } from '../../data.js';
 
 // Components
 import PostCard from '../PostCard/PostCard.js';
 
 const PostList = props => {
 
+    const [ posts, setPosts ] = useState(postsEx)
+    console.log(postsEx)
+    
+    const delPost = (id) => {
+        setPosts([...posts.filter((item) => (
+            item.id !== id
+        ))])}
+
     return (
         <div>
-            <PostCard />
+            { posts.map( item => (
+                <PostCard key={ item.id } 
+                          post={ item } 
+                          setPost={ setPosts }
+                          delPost={ delPost } />
+            ))}
         </div>
     )
 }
